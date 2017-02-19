905df48213267004d775a653413a53af22e26888
Arjemariel Requina <you@example.com>
2017-02-19 16:25:37 +0800

    Tested and Modified the URI Parser function

--- ex1_url_parser/parser.js
+++ ex1_url_parser/parser.js
@@ -20,44 +20,90 @@ function parseUri(uri) {
     var path = "";
     var query = "";
     var fragment = "";
-    uri = uri.substring(uri.indexOf(":") + 3);
+
+    if(uri.indexOf(":") + 1 == uri.indexOf("/")){
+        uri = uri.substring(uri.indexOf(":") + 3);
+    } else {
+        uri = uri.substring(uri.indexOf(":") + 1);
+    }
+   
     if(uri.indexOf("/") >= 0){
         authority = uri.substring(0, uri.indexOf("/"));
         uri = uri.substring(uri.indexOf("/"));
         if(uri.lastIndexOf("/") == uri.indexOf("/")){
-            if(uri.indexOf("?") >= 0 && uri.lastIndexOf("#") > uri.indexOf("?")){
-                if(uri.indexOf("#") >= 0){
-                    query = uri.substring(uri.indexOf("?") + 1, uri.lastIndexOf("#"));
+            if(uri.indexOf("#") >= 0 || uri.indexOf("?") >= 0){
+                var star_idx = uri.indexOf("#");
+                var quest_idx = uri.indexOf("?");
+                if(star_idx >= 0 && quest_idx >= 0){
+                    if(star_idx < quest_idx){
+                      path = uri.substring(0, uri.indexOf("#")); 
+                      fragment = uri.substring(uri.indexOf("#") + 1);
+                    } else {
+                      path = uri.substring(0, uri.indexOf("?")); 
+                      query = uri.substring(quest_idx + 1, star_idx);
+                      fragment = uri.substring(star_idx + 1);
+                    }
+                } else if(star_idx >= 0){
+                    path = uri.substring(0, uri.indexOf("#")); 
                     fragment = uri.substring(uri.indexOf("#") + 1);
-                } else {
+                } else if(quest_idx >= 0){
+                    path = uri.substring(0, uri.indexOf("?")); 
                     query = uri.substring(uri.indexOf("?") + 1);
                 }
-            } else if(uri.indexOf("#") >= 0){
-                fragment = uri.substring(uri.indexOf("#") + 1);
+            } else {
+                path = uri;
             }
         } else {
-            if(uri.lastIndexOf("/") == uri.length - 1){
+            if(uri.lastIndexOf("/") == uri.length - 1 || (uri.indexOf("#") < 0 && uri.indexOf("?") < 0)){
                 path = uri;
             } else {
-                path = uri.substring(0, uri.lastIndexOf("/") + 1);
-                if(uri.indexOf("?") >= 0 && uri.lastIndexOf("#") > uri.indexOf("?")){
-                    if(uri.indexOf("#") >= 0){
-                        query = uri.substring(uri.indexOf("?") + 1, uri.lastIndexOf("#"));
-                        fragment = uri.substring(uri.lastIndexOf("#") + 1);
+                var star_idx = uri.indexOf("#");
+                var quest_idx = uri.indexOf("?");
+                if(star_idx >= 0 && quest_idx >= 0){
+                    if(star_idx < quest_idx){ 
+                      fragment = uri.substring(uri.indexOf("#") + 1);
+                      path = uri.substring(0, uri.indexOf("#"));
                     } else {
-                        query = uri.substring(uri.indexOf("?") + 1);
+                      path = uri.substring(0, uri.indexOf("?"));
+                      query = uri.substring(quest_idx + 1, star_idx);
+                      fragment = uri.substring(star_idx + 1);
+
                     }
-                } if(uri.indexOf("#") >= 0){
+                } else if(star_idx >= 0){
+                    path = uri.substring(0, uri.indexOf("#"));
                     fragment = uri.substring(uri.indexOf("#") + 1);
+                } else if(quest_idx >= 0){
+                    path = uri.substring(0, uri.indexOf("?"));
+                    query = uri.substring(uri.indexOf("?") + 1);
                 }
             }
         }
-      
-        
     } else {
-        authority = uri;
+        if(uri.indexOf("#") >= 0 || uri.indexOf("?") >= 0){ 
+            var star_idx = uri.indexOf("#");
+            var quest_idx = uri.indexOf("?");
+            if(star_idx >= 0 && quest_idx >= 0){
+                if(star_idx < quest_idx){
+                  authority = uri.substring(0, uri.indexOf("#")); 
+                  fragment = uri.substring(uri.indexOf("#") + 1);
+                } else {
+                  authority = uri.substring(0, uri.indexOf("?")); 
+                  query = uri.substring(quest_idx + 1, star_idx);
+                  fragment = uri.substring(star_idx + 1);
+                }
+            } else if(star_idx >= 0){
+                authority = uri.substring(0, uri.indexOf("#")); 
+                fragment = uri.substring(uri.indexOf("#") + 1);
+            } else if(quest_idx >= 0){
+                authority = uri.substring(0, uri.indexOf("?")); 
+                query = uri.substring(uri.indexOf("?") + 1);
+            }
+        } else {
+            authority = uri;
+        }
     }
-    
+
+
     var uriParts = {
         scheme: scheme,
         authority: authority,
